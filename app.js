5d69524efc33a7f1d58398ec185a04b5722cddba
pupupupupupupupupu <arsalanhassan327@gmail.com>
2025-01-27 11:48:49 +0530

    Improved ROCK-PAPER-SCISSORS game with additional Score Tracking and Reset Buttton

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const computerChoiceDisplay = document.getElementById("computer-choice")
 const userChoiceDisplay = document.getElementById("user-choice")
 const resultDisplay = document.getElementById("result")
+const playerScoreDisplay = document.getElementById("player-score")
+const computerScoreDisplay = document.getElementById("computer-score")
 
 const possibleChoices = document.querySelectorAll("button")
 
+let playerScore = 0
+let computerScore = 0
 let userChoice
 let computerChoice
 let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
+    if(possibleChoice.id != reset) {
+        userChoice = e.target.id
+        userChoiceDisplay.innerHTML = userChoice
+        generateComputerChoice()
+        getResult()
+        updateScores()
+    }
 }))
 
 function generateComputerChoice() {
@@ -36,9 +43,29 @@ function getResult() {
         result = 'its a draw!'
     } else if(userChoice === 'paper' && computerChoice === 'rock' || userChoice === 'scissor' && computerChoice === 'paper' || userChoice === 'rock' && computerChoice === 'scissor') {
         result = 'you win!'
+        playerScore++
     } else {
         result = 'you lose!'
+        computerScore++
     }
-    
+
     resultDisplay.innerHTML = result
 }
+
+
+function updateScores() {
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+}
+
+document.getElementById("reset").addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    userChoice = "";
+    computerChoice = "";
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    userChoiceDisplay.textContent = userChoice;
+    computerChoiceDisplay.textContent = computerChoice;
+    resultDisplay.textContent = "Game reset. Start playing!";
+});
